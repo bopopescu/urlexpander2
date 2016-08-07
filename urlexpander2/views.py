7cd851734fd4363fa942c3ca56d8d4f64d135fa0
snitkdan <snitkdan@uw.edu>
2016-08-07 21:23:02 +0000

    Set up index template

--- urlexpander2/views.py
+++ urlexpander2/views.py
@@ -4,7 +4,7 @@ from .models import Url
 
 def index(request):
     all_urls = Url.objects.all()
-    return render(request, 'urlexpander2/index.html', {'all_urls': all_urls})
+    return render(request, 'index.html', {'all_urls': all_urls})
 
 #def detail(request, album_id):
  #   album = get_list_or_404(Album, pk=album_id)
