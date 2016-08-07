cbf6401d1be990b23fe26b08cda9dbc517e18126
snitkdan <snitkdan@uw.edu>
2016-08-07 22:33:54 +0000

    Okay

--- urlexpander2/urls.py
+++ urlexpander2/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     # /urlexpander2/1234
     url(r'^(?P<url_id>[0-9]+)/$', views.detail, name='detail'),
     #/urlexpander2/1234/favorite/
-    url(r'^(?P<url_id>[0-9]+)/$', views.favorite, name = 'favorite')
+    #url(r'^(?P<url_id>[0-9]+)/$', views.favorite, name = 'favorite')
 ]
