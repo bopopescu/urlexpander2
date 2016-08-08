1f76cceef61efd044543df8df16fd1c4a9bf5919
snitkdan <snitkdan@uw.edu>
2016-08-08 13:32:57 -0700

    Revert Urls to working state

--- urlexpander2/models.py
+++ urlexpander2/models.py
@@ -1,19 +1,12 @@
 from django.db import models
 from django.core.urlresolvers import reverse
-import requests, bs4
 
 class Url(models.Model):
     shortened = models.URLField()
-    r = requests.get(shortened)
-
     destination = models.URLField()
-    destination = r.url
-
     status = models.IntegerField()
-    status = r.status_code
-
     title = models.CharField(max_length=500)
-    title = bs4.BeautifulSoup(r.text).title.text
+
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pk': self.pk})
