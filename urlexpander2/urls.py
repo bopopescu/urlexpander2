9665729201eaf4ea034d859e60d96f870e80112d
snitkdan <snitkdan@uw.edu>
2016-08-07 17:00:12 -0700

    Form test

--- urlexpander2/urls.py
+++ urlexpander2/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # /urlexpander2/1234
     url(r'^(?P<url_id>[0-9]+)/$', views.detail, name='detail'),
-    #/urlexpander2/1234/favorite/
-    #url(r'^(?P<url_id>[0-9]+)/$', views.favorite, name = 'favorite')
+    #/urlexpander2/add/
+    #url(r'^add/$', views.add, name = 'add')
 ]
