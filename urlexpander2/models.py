aab9b9c3ee357b95e4568ce3423a9ef9aa9b2bdc
snitkdan <snitkdan@uw.edu>
2016-08-08 13:21:47 -0700

    Updated model to handle only user entered URL

--- urlexpander2/models.py
+++ urlexpander2/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+import requests, bs4
 
 class Url(models.Model):
     shortened = models.URLField()
-    destination = models.URLField()
-    status = models.SmallIntegerField()
-    title = models.CharField(max_length = 500)
+    r = requests.get(shortened)
+
+    destination = r.url
+    status = r.status_code
+
+    beautiful = bs4.BeautifulSoup(r.text)
+    title = beautiful.title.text
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pk': self.pk})
