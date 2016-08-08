965f5767b242266bcfc15c3f350981d0c85021fc
snitkdan <snitkdan@uw.edu>
2016-08-08 10:36:16 -0700

    added user accounts

--- urlexpander2/views.py
+++ urlexpander2/views.py
@@ -1,7 +1,11 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
 from django.core.urlresolvers import reverse_lazy
 from .models import Url
+from .forms import UserForm
 
 class IndexView(generic.ListView):
     template_name = 'urlexpander2/index.html'
@@ -26,4 +30,25 @@ class UrlUpdate(UpdateView):
 class UrlDelete(DeleteView):
     model = Url
     success_url = reverse_lazy('index')
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'urlexpander2/registration_form.html'
+
+    # display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request,self.template_name, {'form': form})
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            # cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
 
