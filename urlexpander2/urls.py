2c40746a0edd75bf285da92ad2dbaa6d19cc65ec
snitkdan <snitkdan@uw.edu>
2016-08-11 15:15:08 -0700

    Back to at leasts

--- urlexpander2/urls.py
+++ urlexpander2/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^', include('django.contrib.auth.urls.login')),
+    url(r'^', include('django.contrib.auth.urls'), name='login'),
     # /urlexpander2/
     url(r'^home$', views.IndexView.as_view(), name='index'),
     #/urlexpander2/register
