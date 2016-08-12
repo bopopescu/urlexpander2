7ddf49ddd9d26b27339ec2dd4f6e0eeae6e1a1c2
snitkdan <snitkdan@uw.edu>
2016-08-12 00:12:13 -0700

    Just so I feel better about myself

--- urlexpander2/models.py
+++ urlexpander2/models.py
@@ -10,8 +10,8 @@ class Url(models.Model):
     snapshot_url = models.CharField(max_length=1000, default='http://google.com')
     timestamp = models.CharField(max_length=15, default='1234567')
 
-    #def get_absolute_url(self):
-     #   return reverse('detail', kwargs={'pk': self.pk})
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return 'Url ' + str(self.id) + ': ' + str(self.shortened)
