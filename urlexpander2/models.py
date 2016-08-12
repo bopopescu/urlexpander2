f235569e2a458689bf06e9b24386556611862f80
snitkdan <snitkdan@uw.edu>
2016-08-12 00:10:05 -0700

    Default values

--- urlexpander2/models.py
+++ urlexpander2/models.py
@@ -7,8 +7,8 @@ class Url(models.Model):
     destination = models.URLField()
     status = models.IntegerField()
     title = models.CharField(max_length=500)
-    snapshot_url = models.CharField(max_length=1000)
-    timestamp = models.CharField(max_length=15)
+    snapshot_url = models.CharField(max_length=1000, default='http://google.com')
+    timestamp = models.CharField(max_length=15, default='1234567')
 
     #def get_absolute_url(self):
      #   return reverse('detail', kwargs={'pk': self.pk})
