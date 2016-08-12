f20448dda0f081f014aa5a9916d0629a8ec608e7
snitkdan <snitkdan@uw.edu>
2016-08-12 00:56:41 -0700

    added to mysite

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^logout/$', include('urlexpander2.urls')),
     url(r'^accounts/login/$', auth_views.login),
     url(r'^', include('urlexpander2.urls')),
 ]
