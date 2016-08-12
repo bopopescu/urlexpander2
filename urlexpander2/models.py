4ca4317f206e7dd9f9468c8fc3f7cba3bab49b99
snitkdan <snitkdan@uw.edu>
2016-08-12 00:05:59 -0700

    Made snapshot muttable

--- urlexpander2/models.py
+++ urlexpander2/models.py
@@ -8,7 +8,7 @@ class Url(models.Model):
     status = models.IntegerField()
     title = models.CharField(max_length=500)
     snapshot_url = models.CharField(max_length=1000)
-    timestamp = models.TextField(max_length=15)
+    timestamp = models.CharField(max_length=15)
 
     #def get_absolute_url(self):
      #   return reverse('detail', kwargs={'pk': self.pk})
