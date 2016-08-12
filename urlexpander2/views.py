8c3c2162e4761256636d2810c281ea19580c5b48
snitkdan <snitkdan@uw.edu>
2016-08-12 00:59:06 -0700

    Rerouted logout to reg page

--- urlexpander2/views.py
+++ urlexpander2/views.py
@@ -42,7 +42,7 @@ def add_url(request):
 @login_required
 def logout_view(request):
     logout(request)
-    return redirect('urlexpander2/index.html')
+    return render(request, 'registration/login.html')
 
 
 class UrlUpdate(UpdateView):
