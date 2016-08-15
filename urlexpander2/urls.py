f0999163a0cc8e16bb4fcd9f474650f323c6e913
snitkdan <snitkdan@uw.edu>
2016-08-15 12:12:14 -0700

    Added auth

--- urlexpander2/urls.py
+++ urlexpander2/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     url(r'^accounts/login/$', auth_views.login),
     url(r'^login_user/$', views.login_user, name='login_user'),
     url(r'^logout_user/$', views.logout_user, name='logout_user'),
-    url(r'^register/$', views.register, name='register'),
 
     # /urlexpander2/
     url(r'^$', views.index, name='index'),
