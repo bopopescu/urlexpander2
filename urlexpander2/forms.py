7b94caf1a406eec63f7eab51239fb7bded00d7fa
snitkdan <snitkdan@uw.edu>
2016-08-17 21:39:16 +0000

    Bring back files

---
+++ urlexpander2/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Url
+from django.contrib.auth.models import User
+
+class UrlForm(forms.ModelForm):
+    class Meta:
+        model = Url
+        fields = ['shortened']
+
+class UrlEditForm(forms.ModelForm):
+    class Meta:
+        model = Url
+        fields = ['shortened', 'destination', 'status', 'title', 'snapshot_url', 'timestamp']
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
---
+++ urlexpander2/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+
+class Url(models.Model):
+    shortened = models.URLField()
+    destination = models.URLField()
+    status = models.IntegerField()
+    title = models.CharField(max_length=500)
+    snapshot_url = models.CharField(max_length=1000, default='http://google.com')
+    timestamp = models.CharField(max_length=15, default='1234567')
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return 'Url ' + str(self.id) + ': ' + str(self.shortened)
+
+
+
+
